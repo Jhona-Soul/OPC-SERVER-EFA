3840fa7868d61ba9069c462d7d5c0dd0bbc7b161
Jhonatan Fulguera <101567189+Jhona-Soul@users.noreply.github.com>
2025-04-14 17:41:57 -0300

    Update sercl.py

--- sercl.py
+++ sercl.py
@@ -22,123 +22,59 @@ idx = server.register_namespace(uri)
 
 objects = server.nodes.objects
 server_interfaces = objects.add_object(idx, "ServerInterfaces")
-
-# Crear objetos y variables
 receta_obj = server_interfaces.add_object(idx, "Server interface_1")
+datos_enviar = receta_obj.add_object(idx, "DATOS OPC A ENVIAR")
+
+lista_alarmas = datos_enviar.add_object(idx, "Alarmas")
+
+grupo_alarmas = [
+    "CANCELACION",
+    "FALLAS CILINDROS",
+    "GENERALES",
+    "INICIO DE CICLO",
+    "POSICIONADOR",
+    "PULSADORES",
+    "ROBOT",
+    "SDDA",
+    "SERVOS"
+]
+
+# Crear subnodos y sus arrays
+for grupo in grupo_alarmas:
+    nodo_grupo = lista_alarmas.add_variable(idx, grupo, 0)
+    
+    # Crear 20 variables (tipo bool) dentro de cada grupo
+    for i in range(20):
+        var = nodo_grupo.add_variable(idx, f"[{i}]", False)
+        var.set_writable() 
+
+Comprobacion_datos = datos_enviar.add_variable(idx, "Confirmacion_envio", True)
+
+variables = []
+
+variables += [
+    Comprobacion_datos.add_variable(idx, "confirmacion_envio",True),
+    Comprobacion_datos.add_variable(idx, "receta_obtenido", 1),
+    Comprobacion_datos.add_variable(idx, "torre_obtenido", 1) 
+]
+
+nivelesHN = datos_enviar.add_variable(idx, "DatosNivelesHN", 0)  # Se crea solo una vez
 variables = [
-    receta_obj.add_variable(idx, "Nombre actual", "Jamon"),
-    receta_obj.add_variable(idx, "PesoProducto", 10.0),  # Peso X FILA de Torre
-    receta_obj.add_variable(idx, "TipoMolde", 1),
-    receta_obj.add_variable(idx, "TotalNiveles", 2.0),
-    receta_obj.add_variable(idx, "idRecetaActual", 1),
-    receta_obj.add_variable(idx, "idRecetaProxima", 1),  
+    nivelesHN.add_variable(idx, "Correccion_hN1", "1"),
+    nivelesHN.add_variable(idx, "Correccion_hN2", "2"),
+    nivelesHN.add_variable(idx, "Correccion_hN3", "3"),
+    nivelesHN.add_variable(idx, "Correccion_hN4", "4"),
+    nivelesHN.add_variable(idx, "Correccion_hN5", "5"),
+    nivelesHN.add_variable(idx, "Correccion_hN6", "6"),
+    nivelesHN.add_variable(idx, "Correccion_hN7", "7"),
+    nivelesHN.add_variable(idx, "Correccion_hN8", "8"),
+    nivelesHN.add_variable(idx, "Correccion_hN9", "9"),
+    nivelesHN.add_variable(idx, "Correccion_hN10", "10"),
+    nivelesHN.add_variable(idx, "Correccion_hN11", "11"),
 ]
 
-variables += [
-    receta_obj.add_variable(idx, "NGripperActual", 2),
-    receta_obj.add_variable(idx, "NGripperProximo", 3),
-]
-
-variables += [
-    receta_obj.add_variable(idx, "posicionX", 20),
-    receta_obj.add_variable(idx, "posicionY", 40),
-    receta_obj.add_variable(idx, "posicionZ", 0),
-]
-
-#datosTorre = server.nodes.objects.add_object(idx, "datosTorre")
-variables += [
-    receta_obj.add_variable(idx, "torreActual", 1),
-    receta_obj.add_variable(idx, "torreProxima", 2),
-]
-#datosSdda = server.nodes.objects.add_object(idx, "datosSdda")
-variables += [
-    receta_obj.add_variable(idx, "nivel_fin",1),
-    receta_obj.add_variable(idx, "sdda_long_mm", 1),
-    receta_obj.add_variable(idx, "sdda_nivel_actual", 1),
-    receta_obj.add_variable(idx, "sdda_vertical_mm", 1),
-]
-
-# Datos adicionales para las alarmas y máquina
-#sector_IO = server.nodes.objects.add_object(idx, "sector_IO")
-variables += [
-    receta_obj.add_variable(idx, "estadoMaquina", 1),
-]
-#ciclo = server.nodes.objects.add_object(idx, "ciclo")
-variables+= [
-    receta_obj.add_variable(idx, "finalizado", True),
-    receta_obj.add_variable(idx, "iniciado", True),
-]
-#desmoldeo = server.nodes.objects.add_object(idx, "desmoldeo")
-variables+= [
-    receta_obj.add_variable(idx, "cicloTiempoTotal", 1),
-    receta_obj.add_variable(idx, "cicloTipoFin", 1),
-    receta_obj.add_variable(idx, "desmoldeobanda",1)
-]
-
-
-
-def crear_variables_nulas(count=11):
-    return []  # Lista vacía, pero con la intención de no superar `count`
-
-server_interface_2 = server_interfaces.add_object(idx, "Server interface_2")
-alarma = server_interface_2.add_object(idx, "Alarma")
-config_obj = server_interface_2.add_object(idx, "Configuraciones")
-
-receta_obj = server_interface_2.add_object(idx, "Receta")  # Se crea solo una vez
-variablesReceta = [
-    receta_obj.add_variable(idx, "NOMBRE", "RECETA MODIFICADA AA"),
-    receta_obj.add_variable(idx, "NUMERO_DE_GRIPPER", 2),
-    receta_obj.add_variable(idx, "TIPO_DE_MOLDE", "Tipo C"),
-    receta_obj.add_variable(idx, "ANCHO_PRODUCTO", 4),
-    receta_obj.add_variable(idx, "ALTO_DE_PRODUCTO", 5),
-    receta_obj.add_variable(idx, "LARGO_DE_PRODUCTO", 6),
-    receta_obj.add_variable(idx, "PESO_DEL_PRODUCTO", 7),
-    receta_obj.add_variable(idx, "MOLDES_POR_NIVEL", 8),
-    receta_obj.add_variable(idx, "ALTO_DE_MOLDE", 9),
-    receta_obj.add_variable(idx, "LARGO_DE_MOLDE", 10),
-    receta_obj.add_variable(idx, "ALTURA_AJUSTE", 11),
-    receta_obj.add_variable(idx, "CANTIDAD_NIVELES", 12),
-    receta_obj.add_variable(idx, "DELTA_ENTRE_NIVELES", 13),
-    receta_obj.add_variable(idx, "ALTURA_N1", 14),
-    receta_obj.add_variable(idx, "ALTURA_DE_BASTIDOR", 15),
-    receta_obj.add_variable(idx, "ALTURA_AJUSTE_N1", 16),
-    receta_obj.add_variable(idx, "torre_proxima", 1),
-    receta_obj.add_variable(idx, "receta_proxima", 21),
-]
-
-
-
-receta_proxima = receta_obj.get_child(f"{idx}:receta_proxima")
-
-def actualizar_receta():
-    while True:
-        nuevo_valor = random.randint(1, 25)
-        receta_proxima.set_value(nuevo_valor)
-        print(f"receta_proxima actualizado a: {nuevo_valor}")
-        time.sleep(60)
-
-thread = Thread(target=actualizar_receta, daemon=True)
-thread.start()
-
-
-
-nivelesHN = server_interface_2.add_object(idx, "DatosNivelesHN")  # Se crea solo una vez
-variablesHN = [
-    nivelesHN.add_variable(idx, "Correcion_hN1", "1"),
-    nivelesHN.add_variable(idx, "Correcion_hN2", "2"),
-    nivelesHN.add_variable(idx, "Correcion_hN3", "3"),
-    nivelesHN.add_variable(idx, "Correcion_hN4", "4"),
-    nivelesHN.add_variable(idx, "Correcion_hN5", "5"),
-    nivelesHN.add_variable(idx, "Correcion_hN6", "6"),
-    nivelesHN.add_variable(idx, "Correcion_hN7", "7"),
-    nivelesHN.add_variable(idx, "Correcion_hN8", "8"),
-    nivelesHN.add_variable(idx, "Correcion_hN9", "9"),
-    nivelesHN.add_variable(idx, "Correcion_hN10", "10"),
-    nivelesHN.add_variable(idx, "Correcion_hN11", "11"),
-]
-
-nivelesuHN = server_interface_2.add_object(idx, "DatosNivelesuHN")  # Se crea solo una vez
-variablesUHN = [
+nivelesuHN = datos_enviar.add_variable(idx, "DatosNivelesuHN", 0)  # Se crea solo una vez
+variables = [
     nivelesuHN.add_variable(idx, "ultimo_hNivel1", "1"),
     nivelesuHN.add_variable(idx, "ultimo_hNivel2", "2"),
     nivelesuHN.add_variable(idx, "ultimo_hNivel3", "3"),
@@ -152,38 +88,38 @@ variablesUHN = [
     nivelesuHN.add_variable(idx, "ultimo_hNivel11", "11"),
 ]
 
-nivelesChG = server_interface_2.add_object(idx, "DatosNivelesChG")  # Se crea solo una vez
-variablesHG = [
-    nivelesChG.add_variable(idx, "Correcion_hguardado_N1", "1"),
-    nivelesChG.add_variable(idx, "Correcion_hguardado_N2", "2"),
-    nivelesChG.add_variable(idx, "Correcion_hguardado_N3", "3"),
-    nivelesChG.add_variable(idx, "Correcion_hguardado_N4", "4"),
-    nivelesChG.add_variable(idx, "Correcion_hguardado_N5", "5"),
-    nivelesChG.add_variable(idx, "Correcion_hguardado_N6", "6"),
-    nivelesChG.add_variable(idx, "Correcion_hguardado_N7", "7"),
-    nivelesChG.add_variable(idx, "Correcion_hguardado_N8", "8"),
-    nivelesChG.add_variable(idx, "Correcion_hguardado_N9", "9"),
-    nivelesChG.add_variable(idx, "Correcion_hguardado_N10", "10"),
-    nivelesChG.add_variable(idx, "Correcion_hguardado_N11", "11"),
+nivelesChG = datos_enviar.add_variable(idx, "DatosNivelesChG",0)  # Se crea solo una vez
+variables = [
+    nivelesChG.add_variable(idx, "Correccion_hguardado_N1", "1"),
+    nivelesChG.add_variable(idx, "Correccion_hguardado_N2", "2"),
+    nivelesChG.add_variable(idx, "Correccion_hguardado_N3", "3"),
+    nivelesChG.add_variable(idx, "Correccion_hguardado_N4", "4"),
+    nivelesChG.add_variable(idx, "Correccion_hguardado_N5", "5"),
+    nivelesChG.add_variable(idx, "Correccion_hguardado_N6", "6"),
+    nivelesChG.add_variable(idx, "Correccion_hguardado_N7", "7"),
+    nivelesChG.add_variable(idx, "Correccion_hguardado_N8", "8"),
+    nivelesChG.add_variable(idx, "Correccion_hguardado_N9", "9"),
+    nivelesChG.add_variable(idx, "Correccion_hguardado_N10", "10"),
+    nivelesChG.add_variable(idx, "Correccion_hguardado_N11", "11"),
 ]
 
-nivelesChB = server_interface_2.add_object(idx, "DatosNivelesChB")  # Se crea solo una vez
-variablesCH8 = [
-    nivelesChB.add_variable(idx, "Correcion_hbusqueda_N1", "1"),
-    nivelesChB.add_variable(idx, "Correcion_hbusqueda_N2", "2"),
-    nivelesChB.add_variable(idx, "Correcion_hbusqueda_N3", "3"),
-    nivelesChB.add_variable(idx, "Correcion_hbusqueda_N4", "4"),
-    nivelesChB.add_variable(idx, "Correcion_hbusqueda_N5", "5"),
-    nivelesChB.add_variable(idx, "Correcion_hbusqueda_N6", "6"),
-    nivelesChB.add_variable(idx, "Correcion_hbusqueda_N7", "7"),
-    nivelesChB.add_variable(idx, "Correcion_hbusqueda_N8", "8"),
-    nivelesChB.add_variable(idx, "Correcion_hbusqueda_N9", "9"),
-    nivelesChB.add_variable(idx, "Correcion_hbusqueda_N10", "10"),
-    nivelesChB.add_variable(idx, "Correcion_hbusqueda_N11", "11"),
+nivelesChB = datos_enviar.add_variable(idx, "DatosNivelesChB",0)  # Se crea solo una vez
+variables = [
+    nivelesChB.add_variable(idx, "Correccion_hbusqueda_N1", "1"),
+    nivelesChB.add_variable(idx, "Correccion_hbusqueda_N2", "2"),
+    nivelesChB.add_variable(idx, "Correccion_hbusqueda_N3", "3"),
+    nivelesChB.add_variable(idx, "Correccion_hbusqueda_N4", "4"),
+    nivelesChB.add_variable(idx, "Correccion_hbusqueda_N5", "5"),
+    nivelesChB.add_variable(idx, "Correccion_hbusqueda_N6", "6"),
+    nivelesChB.add_variable(idx, "Correccion_hbusqueda_N7", "7"),
+    nivelesChB.add_variable(idx, "Correccion_hbusqueda_N8", "8"),
+    nivelesChB.add_variable(idx, "Correccion_hbusqueda_N9", "9"),
+    nivelesChB.add_variable(idx, "Correccion_hbusqueda_N10", "10"),
+    nivelesChB.add_variable(idx, "Correccion_hbusqueda_N11", "11"),
 ]
 
-nivelesFA = server_interface_2.add_object(idx, "DatosNivelesFA")  # Se crea solo una vez
-variablesFA = [
+nivelesFA = datos_enviar.add_variable(idx, "DatosNivelesFA",0)  # Se crea solo una vez
+variables = [
     nivelesFA.add_variable(idx, "FallasN1", "1"),
     nivelesFA.add_variable(idx, "FallasN2", "2"),
     nivelesFA.add_variable(idx, "FallasN3", "3"),
@@ -197,83 +133,103 @@ variablesFA = [
     nivelesFA.add_variable(idx, "FallasN11", "11"),
 ]
 
-DatosTorre = server_interface_2.add_object(idx, "DatosTorre")  # Se crea solo una vez
-variablesTRR = [
-    DatosTorre.add_variable(idx, "TAG", "Cuadrado"),
-    DatosTorre.add_variable(idx, "Coreccion_hBastidor", 1),
-    DatosTorre.add_variable(idx, "Coreccion_hAjuste", 2),
-    DatosTorre.add_variable(idx, "Coreccion_hAjusteN1", 3),
-    DatosTorre.add_variable(idx, "Coreccion_DisteNivel", 4),
+
+estado_equipo = datos_enviar.add_variable(idx, "Estado_equipo", True)
+variables+= [
+    estado_equipo.add_variable(idx, "Ciclo_iniciado", True),
+    estado_equipo.add_variable(idx, "Estado_actual", 1),
+    estado_equipo.add_variable(idx, "Nivel_finalizado", 0)
 ]
 
-for var in receta_obj.get_variables():
-    var.set_writable()
+datos_gripper = datos_enviar.add_variable(idx, "datosGripper", True)
+variables += [
+    datos_gripper.add_variable(idx, "NGripperActual", 2),
+    datos_gripper.add_variable(idx, "NGripperProximo", 3),
+]
 
-for var in DatosTorre.get_variables():
-    var.set_writable()
+datos_robot = datos_enviar.add_variable(idx, "datosRobot", True)
+variables += [
+    datos_robot.add_variable(idx, "posicionX", 20),
+    datos_robot.add_variable(idx, "posicionY", 40),
+    datos_robot.add_variable(idx, "posicionZ", 0),
+]
 
-for var in nivelesHN.get_variables():
-    var.set_writable()
+datos_sdda = datos_enviar.add_variable(idx, "datosSdda", True)
+variables += [
+    datos_sdda.add_variable(idx, "sdda_long_mm", 0),
+    datos_sdda.add_variable(idx, "sdda_nivel_actual", 0),
+    datos_sdda.add_variable(idx, "sdda_vertical_mm", 0)
+]
 
-for var in nivelesuHN.get_variables():
-    var.set_writable()
+datos_seleccionados = datos_enviar.add_variable(idx, "datosSeleccionados", True)
+variables += [
+    datos_seleccionados.add_variable(idx, "N_receta_actual", 0),
+    datos_seleccionados.add_variable(idx, "N_receta_proxima", 0),
+    datos_seleccionados.add_variable(idx, "N_torre_actual", 0),
+    datos_seleccionados.add_variable(idx, "N_torre_proxima", 0),
+    datos_seleccionados.add_variable(idx, "pantalla_receta", True),
+] 
 
-for var in nivelesChG.get_variables():
-    var.set_writable()
+datos_torre = datos_enviar.add_variable(idx, "datosTorre", True)
+variables = [
+    datos_torre.add_variable(idx, "TAG", "Cuadrado"),
+    datos_torre.add_variable(idx, "Correccion_hBastidor", 1),
+    datos_torre.add_variable(idx, "Correccion_hAjuste", 2),
+    datos_torre.add_variable(idx, "Correccion_hAjusteN1", 3),
+    datos_torre.add_variable(idx, "Correccion_DisteNivel", 4),
+]
 
-for var in nivelesChB.get_variables():
-    var.set_writable()
+desmoldeo = datos_enviar.add_variable(idx, "desmoldeo", True)
+variables+= [
+    desmoldeo.add_variable(idx, "cicloTiempoTotal", 1),
+    desmoldeo.add_variable(idx, "cicloTipoFin", 1),
+    desmoldeo.add_variable(idx, "desmoldeobanda",1)
+]
 
-for var in nivelesFA.get_variables():
-    var.set_writable()
-# Crear listas vacías con capacidad de hasta 11 elementos
-max_variables = 11
-DatosNivelesHN = config_obj.add_object(idx, "DatosNivelesHN")
-variables_HN = crear_variables_nulas(max_variables)
 
-DatosNivelesChB = config_obj.add_object(idx, "DatosNivelesChB")
-variables_ChB = crear_variables_nulas(max_variables)
+recetario = datos_enviar.add_variable(idx, "RECETARIO", 0)
 
-DatosNivelesChG = config_obj.add_object(idx, "DatosNivelesChG")
-variables_ChG = crear_variables_nulas(max_variables)
+# Lista de variables por receta
+variables_receta = [
+    "ALTO DE MOLDE",
+    "ALTO DE PRODUCTO",
+    "ALTURA AJUSTE",
+    "ALTURA AJUSTE N1",
+    "ALTURA DE BASTIDOR",
+    "ALTURA N1",
+    "ANCHO PRODUCTO",
+    "CANTIDAD NIVELES",
+    "DELTA ENTRE NIVELES",
+    "LARGO DE MOLDE",
+    "LARGO DE PRODUCTO",
+    "MOLDES POR NIVEL",
+    "NOMBRE",
+    "NUMERO DE GRIPPER",
+    "PESO DEL PRODUCTO",
+    "PRODUCTOS POR MOLDE",
+    "TIPO DE MOLDE"
+]
+RecetaNombres = [
+    "(OV-A) OVALADO A OVA-000",
+    "(OV-B) OVALADO B OVA-001",
+    "(CU) CUADRADO",
+    "(QP) QUESO PUERCO",
+    "(7K) RECTANGULAR",
+    "(6K) MANDOLINA",
+    "(LU) LUNCH"
+]
 
-DatosNivelesFallas = config_obj.add_object(idx, "DatosNivelesFallas")
-variables_Fallas = crear_variables_nulas(max_variables)
 
-DatosNivelesuHN = config_obj.add_object(idx, "DatosNivelesuHN")
-variables_uHN = crear_variables_nulas(max_variables)
+# Crear 20 recetas con las variables
+for i in range(20):
+    receta = recetario.add_variable(idx, f"[{i}]",0)
+    for var_name in variables_receta:
+        if var_name == "NOMBRE":
+            nombre_valor = RecetaNombres[i] if i < len(RecetaNombres) else ""
+            receta.add_variable(idx, var_name, nombre_valor)
+        else:
+            receta.add_variable(idx, var_name, 0)
 
-alarm_nodes = []
-for i in range(0, 50):
-    node = alarma.add_variable(idx, f"[{i}]", False)
-    node.set_writable()
-    alarm_nodes.append(node)
-
-def update_group(start, end, update_interval, hold_time):
-    while True:
-        for i in range(start, end):
-            # Activar el elemento
-            alarm_nodes[i].set_value(True)
-            #print(f"Elemento {i} activado en grupo {start}-{end}")
-            time.sleep(hold_time)
-
-            # Desactivar el elemento
-            alarm_nodes[i].set_value(False)
-            #print(f"Elemento {i} desactivado en grupo {start}-{end}")
-
-            sleep_time = update_interval - hold_time
-            if sleep_time < 0:
-                sleep_time = 0  # Ajustar el tiempo de espera a 0 si es negativo
-            
-            time.sleep(sleep_time)
-# Crear hilos para cada grupo
-thread1 = threading.Thread(target=update_group, args=(0, 15, 15, 3))  # Primeros 15 elementos
-thread2 = threading.Thread(target=update_group, args=(15, 35, 5, 10))  # Siguientes 20 elementos
-thread3 = threading.Thread(target=update_group, args=(35, len(alarm_nodes), 5, 2))  
-            
-# Configurar todas las variables para que sean accesibles desde el cliente
-for var in variables:
-    var.set_writable()
 
 # Función para la simulación del ciclo de desmoldeo
 def ciclo_de_desmoldeo():
@@ -306,36 +262,30 @@ def ciclo_de_desmoldeo():
             print("¡Falla de nivel detectada!")
 
         # Simular el ciclo de desmoldeo de niveles de torre
-        while nivel_actual <= random.randint(9, 11):  # El nivel va hasta 9, 10 o 11
+        while nivel_actual <= random.randint(10, 11):  # El nivel va hasta 9, 10 o 11
             print(f"Procesando torre {torre_actual}, nivel {nivel_actual}")
             estado_maquina = 1
             ciclo_inicio = True
             # Actualizar los valores de los nodos durante el ciclo
             for var in variables:
-                if var.get_browse_name().Name == "Nombre actual":
-                    var.set_value(receta_nombre)
-                elif var.get_browse_name().Name == "RecetaProximaDesmolde":
+                if var.get_browse_name().Name == "N_receta_proxima":
                     var.set_value((idReceta_actual % 7) + 1)
-                elif var.get_browse_name().Name == "PesoProducto":
-                    var.set_value(peso_producto)
                 elif var.get_browse_name().Name == "NGripperActual":
                     var.set_value(gripper_actual)
                 elif var.get_browse_name().Name == "NGripperProximo":
                     var.set_value(gripper_proximo)
-                elif var.get_browse_name().Name == "torreActual":
+                elif var.get_browse_name().Name == "N_torre_actual":
                     var.set_value(torre_actual)
-                elif var.get_browse_name().Name == "torreProxima":
+                elif var.get_browse_name().Name == "N_torre_proxima":
                     var.set_value(torre_proxima)
                 elif var.get_browse_name().Name == "sdda_nivel_actual":
                     var.set_value(nivel_actual)
-                elif var.get_browse_name().Name == "estadoMaquina":
+                elif var.get_browse_name().Name == "Estado_actual":
                     var.set_value(estado_maquina)
-                elif var.get_browse_name().Name == "desmoldeoBanda":
+                elif var.get_browse_name().Name == "desmoldeobanda":
                     var.set_value(desmoldeo_banda)
-                elif var.get_browse_name().Name == "iniciado":
+                elif var.get_browse_name().Name == "Ciclo_iniciado":
                     var.set_value(ciclo_inicio)
-                elif var.get_browse_name().Name == "finalizado":
-                    var.set_value(False)
                 
             print(f"Peso POR nivel: {peso_por_producto} - PesoProducto: {peso_producto} " )
             # Imprimir el estado actual
@@ -416,9 +366,9 @@ if __name__ == "__main__":
         server.start()
 
         iniciar_actualizacion_robot()
-        thread1.start()
-        thread2.start()
-        thread3.start()
+        #thread1.start()
+        #thread2.start()
+        #thread3.start()
         ciclo_de_desmoldeo()
         
     except KeyboardInterrupt:
@@ -426,3 +376,4 @@ if __name__ == "__main__":
     finally:
         server.stop()
         print("Servidor OPC UA apagado.")
+
